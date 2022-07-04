b7287889743891d181f8415e6eb040e5a9f83486
Likhita <leenupandagre24@gmail.com>
2022-07-04 21:55:01 +0530

    all Ucs

---
+++ FindMaximumValue/FindMaximumValue/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+
+        FindMaximumValue.findMaximumValue<int> findMaximumValue = new FindMaximumValue.findMaximumValue<int>(10, 20, 30);
+        findMaximumValue.findMax();
+        FindMaximumValue.findMaximumValue<float> findMaxfloat = new FindMaximumValue.findMaximumValue<float>(10.5F, 20.5F, 30.5F);
+        findMaxfloat.findMax();
+        FindMaximumValue.findMaximumValue<string> findMaxstring = new FindMaximumValue.findMaximumValue<string>("Apple", "Banana", "Orange");
+        findMaxstring.findMax();
+        //new findMaximumValue<float>(10.5F, 20.5F, 30.5F).findMax();
+        // new findMaximumValue<string>("Apple", "Banana", "Orange").findMax();
+    }
+}
---
+++ FindMaximumValue/FindMaximumValue/findmaximumInteger.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumValue
+{
+    public class findMaximumValue<T> where T : IComparable<T>
+    {
+        T firstValue;
+        T secondValue;
+        T thirdValue;
+        public findMaximumValue(T firstValue, T secondValue, T thirdValue)
+        {
+            this.firstValue = firstValue;
+            this.secondValue = secondValue;
+            this.thirdValue = thirdValue;
+
+        }
+        public T findMax()
+        {
+            int intMaxValue;
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
+            {
+                Console.WriteLine(firstValue);
+                return firstValue;
+            }
+            else if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0 ||
+                    secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) > 0 ||
+                    secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) >= 0)
+            {
+                Console.WriteLine(secondValue);
+                return secondValue;
+            }
+            else
+            {
+                Console.WriteLine(thirdValue);
+                return thirdValue;
+            }
+            return firstValue;
+        }
+
+    }
+
+}
